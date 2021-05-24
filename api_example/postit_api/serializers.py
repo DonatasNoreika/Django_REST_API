ba918e61b5b31a1aa8fd4c5b750f88a60296246c
Donoras <donoras@gmail.com>
2021-05-24 11:29:22 +0300

    Initial commit

---
+++ api_example/postit_api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Post, Comment, CommentLike, PostLike
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'title', 'body', 'created']
+
+
