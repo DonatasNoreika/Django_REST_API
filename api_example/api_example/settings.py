32918335f01e06f40ba903b704144a7ca4c4165d
Donoras <donoras@gmail.com>
2021-05-26 11:08:21 +0300

    1-2 done

--- api_example/api_example/settings.py
+++ api_example/api_example/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'postit_api',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -128,3 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
